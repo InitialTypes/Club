76407feaf6edd0fea106061dba0547039457ce68
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-05-20 14:16:33 +0200

    [ cbpv ] some changes I found on my hd

--- lectures/cbpv/Hutton.hs
+++ lectures/cbpv/Hutton.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveFunctor     #-}
 {-# LANGUAGE DeriveFoldable    #-}
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE InstanceSigs #-}
 
 -- | ITC 2021-02-04 Call-by-push-value
 --   Andreas Abel
@@ -23,6 +25,7 @@ instance Applicative Exp' where
 data Exp' a
   = Leaf a
   | Plus (Exp' a) (Exp' a)
+  -- -- | Lambda (Exp' (Maybe a))
   deriving (Eq, Show, Functor, Foldable, Traversable)
 
 type Var = String
@@ -61,7 +64,7 @@ runNum (Leaf n)     = n
 runNum (Plus e1 e2) = runNum e1 + runNum e2
 
 runStr :: Exp' String -> String
-runStr (Leaf s) = s
+runStr (Leaf s)     = s
 runStr (Plus e1 e2) = runStr e1 ++ runStr e2
 
 runMon :: Monoid m => Exp' m -> m
@@ -90,3 +93,34 @@ bind  m f = join $ fmap f m
 
 bindN :: MonadAlgebra m n => m a -> (a -> n  ) -> n
 bindN m f = run  $ fmap f m
+
+instance (MonadAlgebra m n1, MonadAlgebra m n2) => MonadAlgebra m (n1, n2) where
+  run :: m (n1, n2) -> (n1, n2)
+  run m = (run $ fmap fst m, run $ fmap snd m)
+
+instance (MonadAlgebra m n) => MonadAlgebra m (p -> n) where
+  run :: m (p -> n) -> p -> n
+  run m p = run $ fmap ($ p) m
+
+-- instance (MonadAlgebra m n1, MonadAlgebra m n2) => MonadAlgebra m (Either n1 n2) where
+--   run :: m (Either n1 n2) -> Either n1 n2
+--   run m = either
+
+class Comonad d where
+  extract :: d a -> a
+  display :: d a -> d (d a)
+
+-- instance Comonad (i,) where
+--   extract (i,a) = a
+--   display (i,a) = (i,(i,a))
+
+-- instance Monoid j => Comonad (j ->) where
+--   extract f = f mempty
+--   display f = \ j1 -> \ j2 -> f (j1 <> j2)
+
+class (Functor d, Comonad d) => ComonadCoalgebra d p where
+  expose :: p -> d p
+
+instance (ComonadCoalgebra d p1, ComonadCoalgebra d p2) => ComonadCoalgebra d (Either p1 p2) where
+  expose :: Either p1 p2 -> d (Either p1 p2)
+  expose = either (fmap Left . expose) (fmap Right . expose)
